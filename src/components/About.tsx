3c7582a8f8ca4ad0e2b3bd5f017c7de9e1e064cb
ANIRUDDHA ADAK <aniruddhaadak80@gmail.com>
2024-11-13 00:00:12 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -44,8 +44,8 @@ const About: React.FC = () => {
           {/* Profile Image */}
           <div className="md:w-1/2 mb-8 md:mb-0">
             <img
-              src="https://tinyurl.com/2xj2ahws"
-              alt="Profile"
+              src="../../myimage.jpg"
+              alt="MyProfileImage"
               className="rounded-full w-64 h-64 object-cover mx-auto border-4 border-blue-500 shadow-lg transform hover:scale-105 transition-transform duration-300 animate-pulse"
             />
           </div>
